b2faf0dcdfa3332dcc8f41c53e4b056596722891
Mary8992 <mc046569@gmail.com>
2023-03-05 10:11:25 +0300

    menu button with open and close functionality added

---
+++ app.js
@@ -0,0 +1,29 @@
+let menuBtn = document.getElementById("menuBtn");
+let menu = document.getElementById("menu");
+let sideNav = document.getElementById("sideNav");
+
+//1-first way
+sideNav.style.right = "-200px";
+
+function closeOpenBtn () {
+    if (sideNav.style.right == "-200px") {
+        sideNav.style.right = "0";
+        menu.src = 'images/close.png'
+    } else {
+        sideNav.style.right = "-200px";
+        menu.src = 'images/menu.png'
+    }
+};
+
+/*
+//2-second way
+menuBtn.onclick = function () {
+    if (sideNav.style.right == "-200px") {
+        sideNav.style.right = "0";
+    } else {
+        sideNav.style.right = "-200px";
+    }
+};
+
+//in this way we don't need to add onclic="name of function()" anymore
+ */
